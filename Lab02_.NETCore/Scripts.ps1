555eb577bf8cbf830d640f09ea1490c636217b8a
ahmedhassan16 <50599301+ahmedhassan16@users.noreply.github.com>
2021-01-18 19:01:40 -0500

    Lab02 .NET Core

---
+++ Lab02_.NETCore/Scripts.ps1
@@ -0,0 +1,15 @@
+#dotnet --list-sdks
+dotnet new console --output App --framework net5.0 --name NetCore.Docker
+dotnet run --project .\App\
+dotnet publish .\App\ -c Release
+
+
+docker build --file DockerFile --tag core50_image .
+########## .\dive.exe core50_image
+
+#docker run -it -v hostVolume:'c:/Data' --name core50_container core50_image
+docker run  -it `
+            --mount type=bind,source="$(pwd)/hostVolume",target='c:/Data' `
+            --name core50_container core50_image
+            
+docker attach core50_container
