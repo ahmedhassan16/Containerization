945e565437cf28fe6fac8ef1fb35ff55f56fce97
ahmedhassan16 <50599301+ahmedhassan16@users.noreply.github.com>
2021-01-18 19:08:05 -0500

    Lab04 Sitecore 10

---
+++ Lab04_SC10/scripts.ps1
@@ -0,0 +1,9 @@
+﻿#Set-ExecutionPolicy -Scope Process -ExecutionPolicy Bypass
+#netstat -a -b
+.\init.ps1 -LicenseXmlPath "D:\DKR\docker-examples-develop\getting-started\license.xml"
+
+#mkcert -install
+#mkcert -cert-file xp0cm.localhost.crt -key-file xp0cm.localhost.key "xp0cm.localhost"
+#mkcert -cert-file xp0id.localhost.crt -key-file xp0id.localhost.key "xp0id.localhost"
+
+docker-compose up --detach
