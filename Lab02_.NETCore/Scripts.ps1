953cb114d6fc96d23fce0f84c773a127f3043498
ahmedhassan16 <50599301+ahmedhassan16@users.noreply.github.com>
2021-02-13 23:23:35 -0500

    Update Scripts.ps1

--- Lab02_.NETCore/Scripts.ps1
+++ Lab02_.NETCore/Scripts.ps1
@@ -1,11 +1,10 @@
-#dotnet --list-sdks
+# dotnet --list-sdks
 dotnet new console --output App --framework net5.0 --name NetCore.Docker
-#dotnet run --project .\App\             #You have to create c:\data\log.txt & data.txt files
+# dotnet run --project .\App\  "local"           #You have to create c:\data\log.txt & data.txt files
 dotnet publish .\App\ -c Release
 
 
 docker build --file DockerFile --tag core50_image .
-########## .\dive.exe core50_image
 
 #docker run -it -v hostVolume:'c:/Data' --name core50_container core50_image
 docker run  -it `
@@ -13,3 +12,7 @@ docker run  -it `
             --name core50_container core50_image
             
 docker attach core50_container
+
+## Inspect Image        ##### .\dive.exe core50_image
+## Remove Container     ##### docker rm core50_container
+## Remove image         ##### docker rmi core50_image
